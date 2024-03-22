516a8206676754ee410f189c5861455eeb1adedd
Hamish Fleming <fleming_hamish@yahoo.com>
2024-03-22 22:00:56 +1100

    --wip-- [skip ci]

--- lua/example-plugin/fetch.lua
+++ lua/example-plugin/fetch.lua
@@ -1,36 +1,30 @@
 local notify = require("example-plugin.notifier")
+
 local M = {}
 
-
--- This will handle adding new lines if the message is too long
+-- Function to split long messages into lines
 local function split_message(message)
   local lines = {}
-  local line = ''
-  for i = 1, #message do
-    line = line .. message:sub(i, i)
-    if i % 80 == 0 then
-      table.insert(lines, line)
-      line = ''
-    end
+  for i = 1, #message, 80 do
+    table.insert(lines, message:sub(i, i + 79))
   end
-  table.insert(lines, line)
   return lines
 end
 
-
-function M.connect()
-  -- check that the account is set
-  if vim.g.exampleplugin_account == nil then
+function M.connect(account)
+  if not account then
     notify.notify('Account not set')
     return
   end
-  local channel = vim.g.exampleplugin_account
+
   notify.notify('Connecting to channel')
-  local cmd = 'tc connect ' .. channel
+  local cmd = 'tc connect ' .. account
 
   local job_id = vim.fn.jobstart(cmd, {
     on_stdout = function(_, data, _)
-      notify.notify(split_message(data))
+      for _, line in ipairs(split_message(data)) do
+        notify.notify(line)
+      end
     end,
     on_stderr = function(_, data, _)
       notify.notify(data)
