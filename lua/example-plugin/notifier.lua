15b9bf3c4210fb82deface9697bd087e34ffa72b
Hamish Fleming <fleming_hamish@yahoo.com>
2024-03-22 21:18:02 +1100

    --wip-- [skip ci]

--- lua/example-plugin/notifier.lua
+++ lua/example-plugin/notifier.lua
@@ -6,12 +6,7 @@ local M = {}
 function M.notify(message)
     vim.notify_once(
     	message,
-        {
-            title = "Tc Notifications",
-            timeout = 1000,
-            --[[ level = level or vim.log.levels.INFO, ]]
-            level = vim.log.levels.INFO,
-        }
+        level = vim.log.levels.INFO,
     )
 end
 
