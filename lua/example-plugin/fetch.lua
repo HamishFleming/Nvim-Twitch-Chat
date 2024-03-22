2ac4e7267bc86ead6d06145138d92540a8bf66d6
Hamish Fleming <fleming_hamish@yahoo.com>
2024-03-22 21:23:17 +1100

    --wip-- [skip ci]

--- lua/example-plugin/fetch.lua
+++ lua/example-plugin/fetch.lua
@@ -11,11 +11,24 @@ function M.connect()
   notify.notify('Connecting to channel')
   local channel = "V3x_Tech"
   local cmd = 'tc connect ' .. channel
-  local handle = io.popen(cmd)
-  for line in handle:lines() do
-    -- validate the output
-    notify.notify(line)
-  end
+
+  local job_id = vim.fn.jobstart(cmd, {
+    on_stdout = function(_, data, _)
+      notify.notify(data)
+    end,
+    on_stderr = function(_, data, _)
+      notify.notify(data)
+    end,
+    on_exit = function(_, code, _)
+      notify.notify('Exited with code ' .. code)
+    end
+  })
+  --[[ local handle = io.popen(cmd) ]]
+  --[[ for line in handle:lines() do ]]
+  --[[   notify.notify(line) ]]
+  --[[ end ]]
+
+
 end
 
 
