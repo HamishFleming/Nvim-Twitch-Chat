022900e35dfc9a7839167a5a8c4eb08647f90833
Hamish Fleming <fleming_hamish@yahoo.com>
2024-03-22 21:17:18 +1100

    --wip-- [skip ci]

--- lua/example-plugin/fetch.lua
+++ lua/example-plugin/fetch.lua
@@ -8,11 +8,12 @@ local M = {}
 -- run npm command 'tc connect <channel>'
 --[[ function M.connect(channel) ]]
 function M.connect()
-  notify.notify("Connecting to channel")
+  notify.notify('Connecting to channel')
   local channel = "V3x_Tech"
   local cmd = 'tc connect ' .. channel
   local handle = io.popen(cmd)
   for line in handle:lines() do
+    -- validate the output
     notify.notify(line)
   end
 end
--- lua/example-plugin/notifier.lua
+++ lua/example-plugin/notifier.lua
@@ -3,13 +3,14 @@
 
 local M = {}
 
-function M.notify(message, level)
+function M.notify(message)
     vim.notify_once(
     	message,
         {
             title = "Tc Notifications",
             timeout = 1000,
-            level = level or vim.log.levels.INFO,
+            --[[ level = level or vim.log.levels.INFO, ]]
+            level = vim.log.levels.INFO,
         }
     )
 end
