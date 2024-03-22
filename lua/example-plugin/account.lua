c8360ff99d07e49f4416e0b89c7431705441dec4
Hamish Fleming <fleming_hamish@yahoo.com>
2024-03-22 22:36:31 +1100

    --wip-- [skip ci]

--- lua/example-plugin/account.lua
+++ lua/example-plugin/account.lua
@@ -1,12 +1,66 @@
 local vim = vim
 local notify = require("example-plugin.notifier")
+local db = require("example-plugin.db")
+
 local M = {}
 
+-- List all channels in the database
+function M.list_channels()
+    local channels = db.get_channels()
+    for _, channel in ipairs(channels) do
+	notify.notify(channel.name)
+    end
+end
+
+-- List all users in the database
+function M.list_users()
+    local users = db.get_users()
+    for _, user in ipairs(users) do
+	notify.notify(user.name)
+    end
+end
+
+-- List all auth in the database
+function M.list_auth()
+    local auth = db.get_auth()
+    for _, a in ipairs(auth) do
+	notify.notify(a.token)
+    end
+end
+
+-- Add a channel to the database
+function M.add_channel(channel)
+    db.add_channel(channel)
+    notify.notify('Channel ' .. channel .. ' added')
+end
+
+-- Add a user to the database
+function M.add_user()
+    local user = vim.fn.input('Enter user: ')
+    db.add_user(user)
+    notify.notify('User ' .. user .. ' added')
+end
+
+-- Add an auth to the database
+function M.add_auth()
+    local auth = vim.fn.input('Enter auth: ')
+    db.add_auth(auth)
+    notify.notify('Auth ' .. auth .. ' added')
+end
+
+
 -- this will handle setting the account
+--[[ function M.set_account() ]]
+--[[     local account = vim.fn.input('Enter account: ') ]]
+--[[     vim.g.exampleplugin_account = account ]]
+--[[     notify.notify('Account set to ' .. account) ]]
+--[[ end ]]
 function M.set_account()
     local account = vim.fn.input('Enter account: ')
+    add_channel(account)
     vim.g.exampleplugin_account = account
-    notify.notify('Account set to ' .. account)
 end
+
+
 
 return M
---
+++ lua/example-plugin/db.lua
@@ -0,0 +1,78 @@
+
+
+
+
+
+
+
+local sqlite3 = require("ljsqlite3")
+
+
+local M = {}
+
+local db = sqlite3.open("nvim-tc.db")
+
+local function create_table()
+  -- Create channel table
+  db:exec[[
+    CREATE TABLE IF NOT EXISTS twitch_channels (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+    );
+  ]]
+
+  -- Create user table
+  db:exec[[
+    CREATE TABLE IF NOT EXISTS twitch_users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+    );
+  ]]
+
+  -- Create Auth table
+  db:exec[[
+    CREATE TABLE IF NOT EXISTS twitch_auth (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      token TEXT NOT NULL
+    );
+  ]]
+end
+
+function M.get_channels()
+  local channels = {}
+  for row in db:nrows("SELECT * FROM twitch_channels") do
+    table.insert(channels, row)
+  end
+  return channels
+end
+
+
+function M.get_users()
+  local users = {}
+  for row in db:nrows("SELECT * FROM twitch_users") do
+    table.insert(users, row)
+  end
+  return users
+end
+
+function M.get_auth()
+  local auth = {}
+  for row in db:nrows("SELECT * FROM twitch_auth") do
+    table.insert(auth, row)
+  end
+  return auth
+end
+
+function M.add_channel(channel)
+  db:exec("INSERT INTO twitch_channels (name) VALUES ('"..channel.."')")
+end
+
+function M.add_user(user)
+  db:exec("INSERT INTO twitch_users (name) VALUES ('"..user.."')")
+end
+
+function M.add_auth(token)
+  db:exec("INSERT INTO twitch_auth (token) VALUES ('"..token.."')")
+end
+
+return M
