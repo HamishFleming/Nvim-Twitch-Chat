957866c5e68ee5b4b1026ca77a7f26cf9d81bde5
Hamish Fleming <fleming_hamish@yahoo.com>
2024-03-22 21:13:53 +1100

    --wip-- [skip ci]

--- lua/example-plugin/fetch.lua
+++ lua/example-plugin/fetch.lua
@@ -8,10 +8,10 @@ local M = {}
 -- run npm command 'tc connect <channel>'
 --[[ function M.connect(channel) ]]
 function M.connect()
+  notify.notify("Connecting to channel")
   local channel = "V3x_Tech"
   local cmd = 'tc connect ' .. channel
   local handle = io.popen(cmd)
-  -- send every line to the notifier
   for line in handle:lines() do
     notify.notify(line)
   end
