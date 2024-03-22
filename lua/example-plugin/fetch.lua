132b52b932ff2db9415c1e3394b37c2fc1c2b5d9
Hamish Fleming <fleming_hamish@yahoo.com>
2024-03-22 21:06:31 +1100

    --wip-- [skip ci]

--- lua/example-plugin/fetch.lua
+++ lua/example-plugin/fetch.lua
@@ -3,7 +3,9 @@ local M = {}
 
 
 -- run npm command 'tc connect <channel>'
-function M.connect(channel)
+--[[ function M.connect(channel) ]]
+function M.connect()
+  local channel = "V3x_Tech"
   local cmd = 'tc connect ' .. channel
   local handle = io.popen(cmd)
   local result = handle:read("*a")
