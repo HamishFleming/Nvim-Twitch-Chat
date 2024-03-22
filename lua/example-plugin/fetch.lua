c5dbec0a3addd7361389d265e1a55817582d144a
Hamish Fleming <fleming_hamish@yahoo.com>
2024-03-22 21:58:14 +1100

    --wip-- [skip ci]

--- lua/example-plugin/fetch.lua
+++ lua/example-plugin/fetch.lua
@@ -1,6 +1,22 @@
 local notify = require("example-plugin.notifier")
-
 local M = {}
+
+
+-- This will handle adding new lines if the message is too long
+local function split_message(message)
+  local lines = {}
+  local line = ''
+  for i = 1, #message do
+    line = line .. message:sub(i, i)
+    if i % 80 == 0 then
+      table.insert(lines, line)
+      line = ''
+    end
+  end
+  table.insert(lines, line)
+  return lines
+end
+
 
 function M.connect()
   -- check that the account is set
@@ -10,12 +26,14 @@ function M.connect()
   end
   local channel = vim.g.exampleplugin_account
   notify.notify('Connecting to channel')
-  --[[ local channel = "V3x_Tech" ]]
   local cmd = 'tc connect ' .. channel
 
   local job_id = vim.fn.jobstart(cmd, {
     on_stdout = function(_, data, _)
-      notify.notify(data)
+      notify.notify(
+      	        table.concat(split_message(data), '\n')
+	)
+      --[[ data) ]]
     end,
     on_stderr = function(_, data, _)
       notify.notify(data)
@@ -25,6 +43,5 @@ function M.connect()
     end
   })
 end
-
 
 return M
