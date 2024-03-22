7feb308a783f88eef0d51e0ee0c4b6007e0d6c98
Hamish Fleming <fleming_hamish@yahoo.com>
2024-03-22 23:31:10 +1100

    --wip-- [skip ci]

--- lua/example-plugin/db.lua
+++ lua/example-plugin/db.lua
@@ -41,7 +41,7 @@ function M.get_channels()
     local channels = {}
     local rows = execute_query("SELECT name FROM twitch_channels")
     for _, row in ipairs(rows) do
-        table.insert(channels, row.name)
+        table.insert(channels, row[0])
     end
     print(vim.inspect(channels))
     -- Remove duplicates using a Lua table as a set
